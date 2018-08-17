5015c4c6e78f215f839f59e942a56c75d164bbd2
abluchet <abluchet@ds-restauration.com>
2018-08-17 17:04:26 +0200

    lalala

--- src/main.rs
+++ src/main.rs
@@ -1,178 +1,48 @@
-//! Parser example for INI files.
-extern crate combine;
+#[macro_use]
+extern crate nom;
 
-use std::collections::HashMap;
-use std::env;
-use std::fmt;
-use std::fs::File;
-use std::io::{self, Read};
-
-use combine::parser::char::space;
-use combine::stream::state::State;
-use combine::*;
-
-#[cfg(feature = "std")]
-use combine::stream::easy;
-#[cfg(feature = "std")]
-use combine::stream::state::SourcePosition;
-
-enum Error<E> {
-    Io(io::Error),
-    Parse(E),
+#[derive(Debug,PartialEq,Eq)]
+enum SpecialString {
+  Daily,
+  Reboot,
+  Yearly,
+  Weekly,
+  Hourly,
+  Monthly,
+  Annually,
+  Midnight,
+  Unknown(usize)
 }
 
-impl<E> fmt::Display for Error<E>
-where
-    E: fmt::Display,
-{
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            Error::Io(ref err) => write!(f, "{}", err),
-            Error::Parse(ref err) => write!(f, "{}", err),
-        }
-    }
-}
+fn is_not_space(c: u8) -> bool { ! nom::is_space(c)  }
+// fn is_not_eol(c: u8) -> bool { c != b'\n' }
 
-#[derive(PartialEq, Debug)]
-pub struct Ini {
-    pub global: HashMap<String, String>,
-    pub sections: HashMap<String, HashMap<String, String>>,
-}
+named!(special_string<SpecialString>,
+    alt!(
+        tag!("@daily")    => { |_| SpecialString::Daily }    |
+        tag!("@reboot")   => { |_| SpecialString::Reboot }   |
+        tag!("@yearly")   => { |_| SpecialString::Yearly }   |
+        tag!("@weekly")   => { |_| SpecialString::Weekly }   |
+        tag!("@hourly")   => { |_| SpecialString::Hourly }   |
+        tag!("@monthly")  => { |_| SpecialString::Monthly }  |
+        tag!("@annually") => { |_| SpecialString::Annually } |
+        tag!("@midnight") => { |_| SpecialString::Midnight } |
+        take_while!(is_not_space)      => { |r: &[u8]| SpecialString::Unknown(r.len()) }
+    )
+);
 
-fn property<I>() -> impl Parser<Input = I, Output = (String, String)>
-where
-    I: Stream<Item = char>,
-    // Necessary due to rust-lang/rust#24159
-    I::Error: ParseError<I::Item, I::Range, I::Position>,
-{
-    (
-        many1(satisfy(|c| c != '=' && c != '[' && c != ';')),
-        token('='),
-        many1(satisfy(|c| c != '\n' && c != ';')),
-    ).map(|(key, _, value)| (key, value))
-        .message("while parsing property")
-}
+named!(comment<&[u8], (&[u8], &[u8])>, pair!(tag!("#"), take_until!("\n")));
 
-fn whitespace<I>() -> impl Parser<Input = I>
-where
-    I: Stream<Item = char>,
-    I::Error: ParseError<I::Item, I::Range, I::Position>,
-{
-    let comment = (token(';'), skip_many(satisfy(|c| c != '\n'))).map(|_| ());
-    // Wrap the `spaces().or(comment)` in `skip_many` so that it skips alternating whitespace and
-    // comments
-    skip_many(skip_many1(space()).or(comment))
-}
-
-fn properties<I>() -> impl Parser<Input = I, Output = HashMap<String, String>>
-where
-    I: Stream<Item = char>,
-    I::Error: ParseError<I::Item, I::Range, I::Position>,
-{
-    // After each property we skip any whitespace that followed it
-    many(property().skip(whitespace()))
-}
-
-fn section<I>() -> impl Parser<Input = I, Output = (String, HashMap<String, String>)>
-where
-    I: Stream<Item = char>,
-    I::Error: ParseError<I::Item, I::Range, I::Position>,
-{
-    (
-        between(token('['), token(']'), many(satisfy(|c| c != ']'))),
-        whitespace(),
-        properties(),
-    ).map(|(name, _, properties)| (name, properties))
-        .message("while parsing section")
-}
-
-fn ini<I>() -> impl Parser<Input = I, Output = Ini>
-where
-    I: Stream<Item = char>,
-    I::Error: ParseError<I::Item, I::Range, I::Position>,
-{
-    (whitespace(), properties(), many(section())).map(|(_, global, sections)| Ini {
-        global: global,
-        sections: sections,
-    })
-}
-
-// #[test]
-// fn ini_ok() {
-//     let text = r#"
-// language=rust
-//
-// [section]
-// name=combine; Comment
-// type=LL(1)
-//
-// "#;
-//     let mut expected = Ini {
-//         global: HashMap::new(),
-//         sections: HashMap::new(),
-//     };
-//     expected
-//         .global
-//         .insert(String::from("language"), String::from("rust"));
-//
-//     let mut section = HashMap::new();
-//     section.insert(String::from("name"), String::from("combine"));
-//     section.insert(String::from("type"), String::from("LL(1)"));
-//     expected.sections.insert(String::from("section"), section);
-//
-//     let result = ini().parse(text).map(|t| t.0);
-//     assert_eq!(result, Ok(expected));
-// }
-//
-// #[cfg(feature = "std")]
-// #[test]
-// fn ini_error() {
-//     let text = "[error";
-//     let result = ini().easy_parse(State::new(text)).map(|t| t.0);
-//     assert_eq!(
-//         result,
-//         Err(easy::Errors {
-//             position: SourcePosition { line: 1, column: 7 },
-//             errors: vec![
-//                 easy::Error::end_of_input(),
-//                 easy::Error::Expected(']'.into()),
-//                 easy::Error::Message("while parsing section".into()),
-//             ],
-//         })
-//     );
-// }
+// named!(full_greeting<&str, (&str, &str)>,
+//   pair!(get_greeting, complete!(nom::alpha))
+// );
 
 fn main() {
-    let result = match env::args().nth(1) {
-        Some(file) => File::open(file).map_err(Error::Io).and_then(main_),
-        None => main_(io::stdin()),
-    };
-    match result {
-        Ok(_) => println!("OK"),
-        Err(err) => println!("{}", err),
-    }
-}
-
-#[cfg(feature = "std")]
-fn main_<R>(mut read: R) -> Result<(), Error<easy::Errors<char, String, SourcePosition>>>
-where
-    R: Read,
-{
-    let mut text = String::new();
-    read.read_to_string(&mut text).map_err(Error::Io)?;
-    ini()
-        .easy_parse(State::new(&*text))
-        .map_err(|err| Error::Parse(err.map_range(|s| s.to_string())))?;
-    Ok(())
-}
-
-#[cfg(not(feature = "std"))]
-fn main_<R>(mut read: R) -> Result<(), Error<::combine::error::StringStreamError>>
-where
-    R: Read,
-{
-    let mut text = String::new();
-    read.read_to_string(&mut text).map_err(Error::Io)?;
-    ini().parse(State::new(&*text)).map_err(Error::Parse)?;
-    Ok(())
+    let res = special_string(b"@reboot command");
+    println!("{:?}", res);
+    let res = special_string(b"1-3 ");
+    println!("{:?}", res);
+    let comment = comment(b"# lala some comment");
+    println!("{:?}", comment);
+    // println!("result {:?}", full_greeting(" hi bob"));
 }
